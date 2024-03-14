ba1bb9414282af890ed919c07fd8f76404418162
UmeshGayashan <umeshgayashan099@gmail.com>
2024-03-14 21:55:53 +0530

    Route Changes Done

--- routes/healthofficerRoute.js
+++ routes/healthofficerRoute.js
@@ -57,4 +57,58 @@ router.put("/update-parent-acc/:nic", async (req, res) => {
     }
   });
 
+  //Baby Account Creation
+router.post("/create-baby-acc", async (req, res) => {
+  try {
+    const {mfirstName, mlastName, mnic, gender, ofc, birthTime , birthDate ,birthweight , birthHospital} = req.body;
+    const babyId = Math.floor(Math.random() * 100000000);
+
+    const newBabyAcc = new babySchema({
+      babyName: {
+        firstName: mfirstName,
+        lastName: mlastName,
+      },
+      motherorGuardianNIC: mnic,
+      Bid: babyId,
+      gender: gender,
+      ofc: ofc,
+      birthTime:birthTime,
+      birthDate: birthDate,
+      weight: birthweight,
+      hospitalName: birthHospital
+    });
+
+    // Save the new account to the database using async/await
+    const savedBabyAccount = await newBabyAcc.save();
+
+    return res.status(201).send(savedBabyAccount); // Send HTTP 201 for resource creation along with the saved account's data
+  } catch (err) {
+    return res.status(500).send("Account creation failed: " + err.message); // Handle database errors
+  }
+});
+
+//Edit Baby Account
+router.put("/update-baby-acc/:babyId", async (req, res) => {
+  try {
+    const babyId = req.params.babyId;
+    //In request body use Schema Attributes
+    const updateData = req.body;
+    console.log(updateData);
+    const account = await babySchema.findOneAndUpdate(
+      { Bid: babyId },
+      updateData,
+      { new: true }
+    );
+
+    if (!account) {
+      return res.status(404).send("Account not found");
+    }
+
+    return res.status(200).send(account);
+  } catch (err) {
+    return res
+      .status(500)
+      .send("Error while updating account information: " + err.message);
+  }
+});
 module.exports = router; // Export the router instance
