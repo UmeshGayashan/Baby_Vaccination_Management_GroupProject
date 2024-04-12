05e52112a8345064e755237a7552fedb893fed18
UmeshGayashan <umeshgayashan099@gmail.com>
2024-04-12 18:03:42 +0530

    Remote MongoDB Atlas setup instead MongoDB Compass

--- database/connections.js
+++ database/connections.js
@@ -1,13 +1,25 @@
 const mongoose = require("mongoose");
-// const Admin = require("../Schemas/AdminSchema"); // Import the Admin model
-const localDB = "mongodb://localhost:27017/BabyVac";
+
+// // Local Database
+// const localDB = "mongodb://localhost:27017/BabyVac";
+// mongoose.connect(localDB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }).then(() => {
+//     console.log("Connected to database");
+//   }).catch((e) => {
+//     console.log(e);
+//   });
 
 
-mongoose.connect(localDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// Remote Database
+const AtlasDB = "mongodb+srv://umeshgayashan099:Y9r51EWzruEhUbjt@babyvac.rpmitjz.mongodb.net/";
+
+
+mongoose.connect(AtlasDB, {
   }).then(() => {
     console.log("Connected to database");
   }).catch((e) => {
     console.log(e);
   });
+  
