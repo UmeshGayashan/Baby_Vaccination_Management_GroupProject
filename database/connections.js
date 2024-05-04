a192fe97193c9acded3ad4dd889a8752e19caa89
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-05-04 20:06:50 +0530

    MongoDB Atlas Added Correctly

--- database/connections.js
+++ database/connections.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-// // Local Database
-const localDB = "mongodb://localhost:27017/BabyVac";
-mongoose.connect(localDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
-    console.log("Connected to database");
-  }).catch((e) => {
-    console.log(e);
-  });
+// // // Local Database
+// const localDB = "mongodb://localhost:27017/BabyVac";
+// mongoose.connect(localDB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }).then(() => {
+//     console.log("Connected to database");
+//   }).catch((e) => {
+//     console.log(e);
+//   });
 
 
 // // Remote Database
@@ -22,4 +22,16 @@ mongoose.connect(localDB, {
 //   }).catch((e) => {
 //     console.log(e);
 //   });
-  
+
+
+// By Telana
+const mongoose = require("mongoose");
+const AtlasDB = "mongodb+srv://telana:telana@baby-vac.bimois7.mongodb.net/baby-vac?retryWrites=true&w=majority";
+
+
+mongoose.connect(AtlasDB, {
+  }).then(() => {
+    console.log("Connected to database");
+  }).catch((e) => {
+    console.log(e);
+  });
