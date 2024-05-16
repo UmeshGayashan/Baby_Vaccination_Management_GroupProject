50d7563e53611c2347e6654ada4d8739c9fe50bd
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-05-16 11:51:13 +0530

    See vaccinatin details

--- routes/parentRoute.js
+++ routes/parentRoute.js
@@ -22,4 +22,23 @@ router.get("/get/:motherorGuardianNIC", async (req, res) => {
     }
   });
 
+  router.get("/vac/:bid", async (req, res) => {
+    const bid = req.params.bid;
+  
+    try {
+      const vaccinations = await vaccinationSchema.find({ bid });
+  
+      if (!vaccinations || vaccinations.length === 0) {
+        return res.status(404).json({ error: "No Vaccinations found" });
+      }
+  
+      return res.status(200).json(vaccinations);
+    } catch (err) {
+      return res.status(500).json({
+        error: "Error while fetching babies",
+        message: err.message,
+      });
+    }
+  });
+
 module.exports = router; // Export the router instance
