f8fa8f7d830ab2ea4d34e97f26e9633a91a45dec
Telana99 <139704352+Telana99@users.noreply.github.com>
2024-06-18 14:51:51 +0530

    update & delete health account is done

--- frontend/src/pages/HighAdmin/UpdateProffessions.js
+++ frontend/src/pages/HighAdmin/UpdateProffessions.js
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import "../pageCss/AddPerant.css";
 import HomeLink from "../../components/HomeLink";
@@ -13,30 +14,172 @@ import HAAProNavbar from "../../components/ADDprofessionnavbar";
 const UpdateProffessions = () => {
 
   const navigate = useNavigate();
-  const [orderNotes, setOrderNotes] = useState("");
-  const [selectedImage, setSelectedImage] = useState(null);
 
-  const onButtons1Click = useCallback(() => {
+  const onNavButtons1Click = useCallback(() => {
     navigate("/high-admin-proffessions");
   }, [navigate]);
 
+  //==========
+
+  const [hcpNIC, setHcpNIC] = useState("");
+  const [hcpData, setHcpData] = useState({
+    firstName: "",
+    lastName: "",
+    nic: "",
+    postalCode: "",
+    telephone: "",
+    position: "",
+    age: "",
+    email: "",
+    password: "",
+    username: "",
+    address: "",
+    additionalInfo: ""
+  });
+
+//================================
+  const [orderNotes, setOrderNotes] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+ //======================
+  const handleFetchHcpInfo = () => {
+
+    const url = `http://localhost:4000/admin/get-health-acc/${hcpNIC}`; 
+    axios.get(url)
+     .then(response => {
+        const data = response.data;
+        setHcpData({
+          firstName: data.hcpName.firstName,
+          lastName: data.hcpName.lastName,
+          nic: data.hcpNIC,
+          postalCode: data.hcpPostalCode,
+          telephone: data. hcpTelephone,
+          position: data.hcpPosition,
+          age: data.hcpAge,
+          email: data. hcpEmail,
+          password:  " ",
+          username: data.username,
+          address: data.hcpAddress,
+          additionalInfo: data.hcpinfo
+        });
+      })
+     .catch(error => {
+        console.error("Error fetching Healthcare prof. information: ",error);
+      });
+  };
+
+  //update hcp information
+
+  const handleUpdateHcpInfo =() => {
+    const url = `http://localhost:4000/admin/update-health-acc/${hcpNIC}`;
+    axios.put(url, {
+      hcpName: {
+        firstName: hcpData.firstName,
+        lastName: hcpData.lastName
+      },
+      hcpNIC: hcpData.nic,
+      hcpPostalCode: hcpData.postalCode,
+      hcpTelephone: hcpData.telephone,
+      hcpPosition: hcpData.position,
+      hcpAge: hcpData.age,
+      hcpEmail: hcpData.email,
+      username: hcpData.username,
+      hcpAddress: hcpData.address,
+      hcpinfo: hcpData.additionalInfo,
+      password: hcpData.password,
+     
+    }). then(response => {
+      console.log("Healthcare prof. information updated successfully:", response.data);
+      navigate("/high-admin-proffessions");
+    })
+    .catch(error => {
+      console.error("Error updating Healthcare prof. information:", error);
+    });
+
+  };
+
+  //delete hcp account
+  const handleDeleteHcpInfo = () => {
+    const url = `http://localhost:4000/admin/delete-health-acc/${hcpNIC}`;
+    axios.delete(url)
+     .then(response => {
+        console.log("Healthcare prof. account deleted successfully:", response.data);
+        navigate("/high-admin-proffessions");
+      })
+     .catch(error => {
+        console.error("Error deleting Healthcare prof. account:", error);
+      });
+  };
+
+  const onButtons1Click = useCallback( () => {
+    navigate("/high-admin-proffessions");
+  }, [navigate]);
+
+ 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedImage(file);
   };
 
-  const onNavButtonClick = useCallback(() => {
-    navigate("/high-admin-proffessions");
-  }, [navigate]);
+
 
   return (
     <div className="add-perant">
       {/* navbar */}
       <HAAProNavbar/>
-      <HomeLink addChild="Update Helth Care Proffessions" />
+      <HomeLink addChild="Update Health Care Proffesions" />
 
       <section className="blog-link">
         <div className="linked-in-link" />
+      </section>
+
+
+      
+    <section className="facebook-link" style={{marginLeft:"50px"}}>
+        <div className="instagram-link">
+          <div className="text-input">
+            <div className="icon-container">
+              <div className="password3">
+                <div className="input-text-label3" style={{ fontSize: '20px', marginBottom: "10px" }}>Healthcare NIC</div>
+                <div className="input-field4" style={{ marginBottom: "20px" }}>
+                  <div className="text3">
+                    <input
+                      className="type-here6"
+                      placeholder="Enter Healthcare Proffesion's NIC"
+                      type="text"
+                      value={hcpNIC}
+                      onChange={(e) => setHcpNIC(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <Button className="primary-button"
+                  variant="contained"
+                  sx={{
+                    textTransform: "none",
+                    color: "#000",
+                    fontSize: "16",
+                    background: "#f2c94c",
+                    borderRadius: "4px",
+                    "&:hover": { background: "#f2c94c" },
+                    width: 187,
+                    height: 51,
+                  }} onClick={handleFetchHcpInfo}>Get Account Info</Button>
+              <Button className="primary-button"
+                  variant="contained"
+                  sx={{
+                    textTransform: "none",
+                    color: "#fff",
+                    fontSize: "16",
+                    background: "#f44336",
+                    borderRadius: "4px",
+                    "&:hover": { background: "#d32f2f" },
+                    width: 187,
+                    height: 51,
+                  }} onClick={handleDeleteHcpInfo}>Delete Healthcare Proffesion</Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section className="mothers-name-field">
@@ -56,6 +199,8 @@ const UpdateProffessions = () => {
                 className="filledgmailcom"
                 placeholder="Your first name"
                 type="text"
+                value={hcpData.firstName}
+                onChange={(e) => setHcpData({...hcpData, firstName: e.target.value})}
               />
             </div>
           </div>
@@ -71,6 +216,8 @@ const UpdateProffessions = () => {
               className="filledgmailcom1"
               placeholder="Your last name"
               type="text"
+              value={hcpData.lastName}
+              onChange={(e) => setHcpData({...hcpData, lastName: e.target.value})}
             />
           </div>
         </div>
@@ -83,6 +230,8 @@ const UpdateProffessions = () => {
               className="filledgmailcom2"
               placeholder="NIc No"
               type="text"
+              value={hcpData.nic}
+              onChange={(e) => setHcpData({...hcpData, nic: e.target.value})}
             />
           </div>
         </div>
@@ -94,54 +243,71 @@ const UpdateProffessions = () => {
               className="filledgmailcom2"
               placeholder="Address"
               type="text"
+              value={hcpData.address}
+              onChange={(e) => setHcpData({...hcpData, address: e.target.value})}
             />
           </div>
       <div className="footer-frame1">
         <div className="postal-code">Postal Code</div>
         <div className="input-field63">
-          <input className="code" placeholder="Code" type="text" />
+          <input className="code" placeholder="Code" type="text"
+          value={hcpData.postalCode} 
+          onChange={(e) => setHcpData({...hcpData, postalcode: e.target.value})}
+           />
         </div>
       </div>
 
       <div className="footer-frame1">
-        <div className="postal-code">Possition</div>
+        <div className="postal-code">Position</div>
         <div className="input-field63">
-          <input className="code" placeholder="Posion" type="text" />
+          <input className="code" placeholder="Position" type="text"
+          value={hcpData.position}
+          onChange={(e) => setHcpData({...hcpData, position: e.target.value})} />
         </div>
       </div>
 
             <div className="footer-frame1">
         <div className="postal-code">Age</div>
         <div className="input-field63">
-          <input className="code" placeholder="Age" type="text" />
+          <input className="code" placeholder="Age" type="text"
+          value = {hcpData.age}
+          onChange={(e) => setHcpData({...hcpData, age: e.target.value})}/>
         </div>
       </div>
 
       <div className="footer-frame1">
         <div className="postal-code">Email</div>
         <div className="input-field63">
-          <input className="code" placeholder="Email Address" type="Email" />
+          <input className="code" placeholder="Email Address" type="Email" 
+          value={hcpData.email}
+          onChange={(e)=> setHcpData({...hcpData, email: e.target.value })}/>
         </div>
       </div>
 
       <div className="footer-frame1">
         <div className="postal-code">Phone Number</div>
         <div className="input-field63">
-          <input className="code" placeholder="Phone number" type="number" />
+          <input className="code" placeholder="Phone number" type="number"
+          value= {hcpData.telephone} 
+          onChange={(e) => setHcpData({...hcpData, telephone: e.target.value})}/>
         </div>
       </div>
 
       <div className="footer-frame1">
         <div className="postal-code">User Name</div>
         <div className="input-field63">
-          <input className="code" placeholder="User Name" type="text" />
+          <input className="code" placeholder="User Name" type="text" 
+          value={hcpData.username}
+          onChange={(e) => setHcpData({...hcpData, username: e.target.value})}/>
         </div>
       </div>
 
       <div className="footer-frame1">
         <div className="postal-code">Password</div>
         <div className="input-field63">
-          <input className="code" placeholder="Password" type="text" />
+          <input className="code" placeholder="Password" type="text"
+          value={hcpData.password}
+          onChange={(e) => setHcpData({...hcpData, password:e.target.value})} />
         </div>
       </div>
 
@@ -158,8 +324,8 @@ const UpdateProffessions = () => {
         <textarea
           className="filledgmailcom3"
           placeholder="Notes about your order, e.g. special notes for delivery"
-          value={orderNotes}
-          onChange={(e) => setOrderNotes(e.target.value)}
+          value={hcpData.additionalInfo}
+          onChange={(e) => setHcpData({...hcpData, additionalInfo:e.target.value})}
           style={{ width: "100%", height: "150px" ,marginBottom:"-50px" }}
         />
       </div>
@@ -235,7 +401,7 @@ const UpdateProffessions = () => {
             width: 172,
             height: 56,
           }}
-          onClick={onButtons1Click}
+          onClick={handleUpdateHcpInfo}
         >
           Save
         </Button>
